a52b576e0ed4df8c29fec783f692f4dcc7865599
Dmytrenko-Roman <savitarreus@gmail.com>
2022-05-23 16:43:14 +0300

    ❄️ Scrapped hero data ❄️

--- hots/spiders/heroes.py
+++ hots/spiders/heroes.py
@@ -1,4 +1,5 @@
 import scrapy
+import json
 
 
 class HeroesSpider(scrapy.Spider):
@@ -6,4 +7,23 @@ class HeroesSpider(scrapy.Spider):
     start_urls = ["https://heroesofthestorm.com/en-gb/heroes/"]
 
     def parse(self, response):
-        pass
+        script_with_heroes = response.css("script[type=\"text/javascript\"]::text").getall()[-1]
+        heroes_data = self.get_javascript_data(script_with_heroes, "window.blizzard.hgs.heroData")
+
+        for hero in heroes_data:
+            yield hero
+
+    def get_javascript_data(self, html: str, variable_name: str) -> json:
+        """
+        get_javascript_data finds variable in javascript data.
+
+        :param html: string to find data
+        :param variable_name: name of the variable to find
+        :return: json
+        """
+
+        start_from = html.index("=", html.index(variable_name)) + 1
+        ends_on = html.index("];", html.index(variable_name)) + 1
+
+        data = json.loads(html[start_from:ends_on])
+        return data
