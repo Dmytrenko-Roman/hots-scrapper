193da3f0da4fde72071c1d9a6ba9677b8b5be810
Dmytrenko-Roman <savitarreus@gmail.com>
2022-06-04 17:39:42 +0300

    ❄️ Update serializers ❄️

--- api/heroes/migrations/0001_initial.py
+++ api/heroes/migrations/0001_initial.py
@@ -7,26 +7,33 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Hero',
+            name="Hero",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=100)),
-                ('title', models.CharField(max_length=200)),
-                ('role', models.CharField(max_length=10)),
-                ('type', models.CharField(max_length=50)),
-                ('description', models.TextField()),
-                ('difficulty', models.CharField(max_length=10)),
-                ('card_portrait', models.CharField(max_length=100)),
-                ('franchise', models.CharField(max_length=20)),
-                ('href', models.CharField(max_length=100)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=100)),
+                ("title", models.CharField(max_length=200)),
+                ("role", models.CharField(max_length=10)),
+                ("type", models.CharField(max_length=50)),
+                ("description", models.TextField()),
+                ("difficulty", models.CharField(max_length=10)),
+                ("card_portrait", models.CharField(max_length=100)),
+                ("franchise", models.CharField(max_length=20)),
+                ("href", models.CharField(max_length=100)),
             ],
             options={
-                'ordering': ['franchise'],
+                "ordering": ["franchise"],
             },
         ),
     ]
--- api/heroes/serializers.py
+++ api/heroes/serializers.py
@@ -2,17 +2,20 @@ from rest_framework import serializers
 from heroes.models import Hero
 
 
-class HeroSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(required=True, allow_blank=False, max_length=100)
-    title = serializers.CharField(required=True, allow_blank=False, max_length=200)
-    role = serializers.CharField(required=True, allow_blank=False, max_length=10)
-    type = serializers.CharField(required=True, allow_blank=False, max_length=50)
-    description = serializers.CharField(style={'base_template': 'textarea.html'})
-    difficulty = serializers.CharField(required=True, allow_blank=False, max_length=10)
-    card_portrait = serializers.CharField(required=True, allow_blank=False, max_length=100)
-    franchise = serializers.CharField(required=True, allow_blank=False, max_length=20)
-    href = serializers.CharField(required=True, allow_blank=False, max_length=100)
+class HeroSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hero
+        fields = [
+            "name",
+            "title",
+            "role",
+            "type",
+            "description",
+            "difficulty",
+            "card_portrait",
+            "franchise",
+            "href",
+        ]
 
     def create(self, validated_data):
         return Hero.objects.create(**validated_data)
